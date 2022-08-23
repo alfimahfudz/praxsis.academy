f92daf1164d5d6b8af25719019573bc7935eecca
alfimahfudz <alfimahfudzoh@gmail.com>
2022-08-23 13:44:02 +0700

    first commit

---
+++ novice/02.02/4.2 for statment/index.py
@@ -0,0 +1,22 @@
+# word =("anggur", "apel", "jeruk")
+# for i in word:
+#     print(i)
+users = {'titin': 'active', 'vena': 'inactive', 'fitra': 'active'}
+
+for user, status in users.copy().items():
+    if status == 'inactive':
+       del users[user]
+
+
+active_users = {}
+for user, status in users.items():
+    if status == 'active':
+       active_users[user] = status
+
+print(users)     
+print(active_users)
+
+a = {}
+a["c"] = "d"
+a["e"] = "f"
+print(a)
---
+++ novice/02.02/4.3 The range() Function/index.py
@@ -0,0 +1,17 @@
+# for i in range(5):
+#     print(i)
+print(list(range(5, 10)))
+[5, 6, 7, 8, 9]
+print(list(range(0, 10, 3)))
+[0, 3, 6, 9]
+print(list(range(-10, -100, -30)))
+[-10, -40, -70]
+
+# def buah(c):
+#     print(f"ini buah mangga rasanya {c}")
+# buah("manis")
+a = ['mary', 'had', 'a', 'little', 'lamb']
+for i in range(len(a)):
+    print(i, a[i])
+print(range(10))
+print(sum(range(4)))
