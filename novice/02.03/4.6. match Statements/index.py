91895410ee4bf1fa6a094b7e7b236f82b018f412
alfimahfudz <alfimahfudzoh@gmail.com>
2022-08-24 12:30:04 +0700

    first commit

--- novice/02.03/4.6. match Statements/index.py
+++ novice/02.03/4.6. match Statements/index.py
@@ -6,10 +6,43 @@ def http_error(status):
             return "Not found"
         case 418:
             return "I'm a teapot"
+        case 401 | 403 | 404: 
+            return "Not allowed" 
         case _:
-            return "Somethings wrong with the internet" 
-        case 401 | 403 | 404:  
+            return "Somethings wrong with the internet"    
+print(http_error(550))
 
-print(http_error(404))
+# point is an (x, y) tuple
+def satu(point):
+    match point:
+        case (0, 0):
+            print("Origin")
+        case (0, y):
+            print(f"Y={y}")
+        case (x, 0):
+            print(f"X={x}")
+        case (x, y):
+            print(f"X={x}, Y={y}")
+        case _:
+             raise ValueError("Not a point")
+class Point:
+    x: int
+    y: int
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+def where_is(point):
+    match point:
+        case Point(x=0, y=0):
+            print("Origin")
+        case Point(x=0, y=y):
+            print(f"Y={y}")
+        case Point(x=x, y=0):
+            print(f"X={x}")
+        case Point():
+            print("Somewhere else")
+        case _:
+            print("Not a point")
 
+where_is(point(0,6))                    
  
--- novice/02.03/4.8. More on Defining Functions¶/4.8.2. Keyword Arguments/index.py
+++ novice/02.03/4.8. More on Defining Functions¶/4.8.2. Keyword Arguments/index.py
@@ -9,8 +9,25 @@ parrot(voltage=1000000, action='VOOOOOM')             # 2 keyword arguments
 parrot(action='VOOOOOM', voltage=1000000)             # 2 keyword arguments
 parrot('a million', 'bereft of life', 'jump')         # 3 positional arguments
 parrot('a thousand', state='pushing up the daisies')  # 1 positional, 1 keyword    
+parrot(1000)                     # required argument missing
+parrot(5.0, 'dead')  # non-keyword argument after a keyword argument
+# 
 
-parrot()                     # required argument missing
-parrot(voltage=5.0, 'dead')  # non-keyword argument after a keyword argument
-parrot(110, voltage=220)     # duplicate value for the same argument
-parrot(actor='John Cleese')  # unknown keyword argument
+def cheeseshop(kind, *arguments, **keywords):
+    print("-- Do you have any", kind, "?")
+    print("-- I'm sorry, we're all out of", kind)
+    for arg in arguments:
+        print(arg)
+    print("-" * 40)
+    for kw in keywords:
+        print(kw, ":", keywords[kw])
+
+cheeseshop("Limburger", "It's very runny, sir.",
+           "It's really very, VERY runny, sir.",
+           shopkeeper="Michael Palin",
+           client="John Cleese",
+           sketch="Cheese Shop Sketch")
+
+def function(a):
+    pass
+# function(0, a=0)
