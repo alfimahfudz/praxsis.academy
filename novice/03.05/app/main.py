42a89b9b6e803204cedb8bbf5ad588970419ca61
alfimahfudz <alfimahfudzoh@gmail.com>
2022-09-05 12:01:24 +0700

    first commit

--- novice/03.05/app/main.py
+++ novice/03.05/app/main.py
@@ -1,6 +1,8 @@
 # from crypt import methods
+from ast import Delete
 from flask import Flask, render_template
 from flask import request
+from flask import redirect
 import psycopg2 
 app = Flask(__name__)
 
@@ -11,7 +13,6 @@ def index():
         database="contoh",
         user="postgres",
         password="Alfi12345")
-
     curs = conn.cursor()
 
     if request.method == "POST":
@@ -36,6 +37,41 @@ def index():
     data = curs.fetchall() 
     # data = ["apel", "pear", "anggur"]
     return render_template("index.html", context=data)
+@app.route("/detail/<buah_id>")
+def detail(buah_id):
+    conn = psycopg2.connect(
+        host="localhost",
+        database="contoh",
+        user="postgres",
+        password="Alfi12345")
+
+    curs = conn.cursor()
+    query = f"select * from buah where id = {buah_id}"
+    curs.execute(query)
+    data = curs.fetchone()
+    curs.close()
+    conn.close()
+    print(data)
+    return render_template("detail.html", context=data)
+
+@app.route("/delete/<buah_id>")
+def delete(buah_id):
+    conn = psycopg2.connect(
+        host="localhost",
+        database="contoh",
+        user="postgres",
+        password="Alfi12345")
+
+    curs = conn.cursor()
+    query = f"delete from buah where id = {buah_id}"
+    curs.execute(query)
+    conn.commit()
+    curs.close()
+    conn.close()
+    
+    return redirect("/")
+            
+        
 
 if __name__ =="__main__":    
     app.run()
